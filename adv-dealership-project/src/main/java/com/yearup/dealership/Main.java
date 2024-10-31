e446be22b585e61b531d930e03fd528060ce92ca
Fran-Santos <francihelen.santos25@gmail.com>
2024-10-30 22:54:51 -0700

    initial commit

---
+++ adv-dealership-project/src/main/java/com/yearup/dealership/Main.java
@@ -0,0 +1,8 @@
+package com.yearup.dealership;
+
+public class Main {
+    public static void main(String[] args) {
+        UserInterface myUI = new UserInterface();
+        myUI.display();
+    }
+}
